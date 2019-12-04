a807ca083dad0d946aa840aae8fbca7a461075e0
Javlonbek Butabaev <jbutabaev@gmail.com>
2019-12-04 19:31:27 +0500

    fix port num

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "build")));
 
